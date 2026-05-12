a892ab9b1fe15f773069787153f50f56587ea545
Evelina <evelinaavegas@gmail.com>
2026-05-12 18:39:01 +0300

    marker add

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { cardsArr } from "./cards-create.js";
 import { displayCards } from "./cards-display.js";
 import { priseCorecting, selectedTime} from "./cards-filtering.js";
-import { form } from "./create-map.js";
+import {  form,  mapFilters,  disabledForm, map } from "./create-map.js";
 
 
 displayCards(cardsArr)
@@ -23,4 +23,22 @@ timeIn.addEventListener('change', (e)=>{
 timeOut.addEventListener('change', (e)=>{
     let target = e.target.value
     selectedTime(target, timeIn)
+})
+
+// Leaflet map create
+
+const formAtributs = document.querySelectorAll('.ad-form fieldset')
+const filterAtributs = document.querySelectorAll('.map__filters select, fieldset')
+
+disabledForm(filterAtributs, true)
+disabledForm(formAtributs, true)
+
+map.whenReady(() => {
+    form.classList.remove('ad-form-disabled')
+    mapFilters.classList.remove('ad-form-disabled')
+    setTimeout(() => {
+        disabledForm(filterAtributs, false)
+        disabledForm(formAtributs, false)
+    }, 1000);
+    
 })
